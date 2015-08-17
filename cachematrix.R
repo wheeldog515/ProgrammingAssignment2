f4832e3df3c2c669886fd3803fd85af5e61f124b
wheeldog515 <andrew.wheeler@SYD-D70000816.syd.ads.finisar.com>
2015-08-17 14:31:46 +1000

    Completed Assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Allows a matricies to store their inverses, to prevent
+## repeated calculations of said inverse
 
-## Write a short comment describing this function
+## Creates a special "matrix" capable of storing its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(mean) m <<- mean
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Enables calculation, storage and detection of stored inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  return(m)
 }
